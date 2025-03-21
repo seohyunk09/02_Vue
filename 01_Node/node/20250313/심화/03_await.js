ad546378303a02a30f843d72e4196daf698b1cd1
seohyunk09 <2022112400@dgu.ac.kr>
2025-03-21 16:32:59 +0900

    site.html 수정

---
+++ 01_Node/node/20250313/심화/03_await.js
@@ -0,0 +1,11 @@
+const fs = require('fs').promises;
+
+async function readDirAsyn() {
+  try {
+    const files = await fs.readdir('./');
+    console.log(files);
+  } catch (err) {
+    console.error(err);
+  }
+}
+readDirAsyn();
