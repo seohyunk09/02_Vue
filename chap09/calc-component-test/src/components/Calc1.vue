0c3ebce4eafd987a817dc9b116868abb02972b94
seohyunk09 <2022112400@dgu.ac.kr>
2025-03-27 15:08:17 +0900

    기본 문제 calc-component-test 파일 추가

---
+++ chap09/calc-component-test/src/components/Calc1.vue
@@ -0,0 +1,45 @@
+<!-- Options API 적용 -->
+<template>
+  <div>
+    <h3>Options API</h3>
+    <input
+      type="text"
+      placeholder="입력하면 대문자로 변환"
+      :value="name"
+      @input="changeName($event.target.value)"
+    /><!-- 콜론?? V-BIND (단방향!!Model=> View ) -->
+  </div>
+  <div>
+    X : <input type="text" v-model.number="x" /> <br />
+    Y : <input type="text" v-model.number="y" /> <br />
+    <button @click="calculateSum(x, y)">계산하기</button>
+    <p>X+Y = {{ sum1 }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Calc1', // 컴포넌트의 이름을 지정하는 것이 name 옵션!!
+  data() {
+    return {
+      //name 관련 기능
+      name: '',
+      // 계산 관련 기능
+      x: 10,
+      y: 20,
+      sum1: 30,
+    };
+  },
+
+  methods: {
+    // name 관련 메서드
+    changeName(inputName) {
+      this.name = inputName.toUppercase(); //대문자 변환
+    },
+    //계산 관련 메서드
+    calculateSum(num1, num2) {
+      this.sum1 = num1 + num2;
+    },
+  },
+};
+</script>
---
+++ chap09/calc-component-test/src/components/Calc2.vue
@@ -0,0 +1,46 @@
+<!-- Composition API 적용 -->
+<template>
+  <div>
+    <h3>Composition API</h3>
+    <input
+      type="text"
+      placeholder="입력하면 대문자로 변환"
+      :value="name"
+      @input="changeName($event.target.value)"
+    /><!-- 콜론?? V-BIND (단방향!!Model=> View ) -->
+  </div>
+  <div>
+    X : <input type="text" v-model.number="x" /> <br />
+    Y : <input type="text" v-model.number="y" /> <br />
+    <button @click="calculateSum(x, y)">계산하기</button>
+    <p>X+Y = {{ sum1 }}</p>
+  </div>
+</template>
+<script>
+import { ref } from 'vue';
+export default {
+  name: 'Calc2',
+  setup() {
+    /* name관련  data, method */
+    const name = ref(''); //data() 대체, 값 접근 시 .value 이용!
+
+    const changeName = (inputName) => {
+      name.value = inputName.toUpperCase();
+    };
+
+    //----------------------------------------------
+    /* 계산 관련 method,data */
+
+    const x = ref(5);
+    const y = ref(7);
+    const sum1 = ref(12);
+
+    const calculateSum = (num1, num2) => {
+      sum1.value = num1 + num2;
+    };
+
+    //setup() return 구문에 작성 - <template>에서  사용 가능
+    return { name, changeName, x, y, sum1, calculateSum };
+  },
+};
+</script>
